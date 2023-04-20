b09be3affb1ef8a47d936328c546a35bca720612
Ebenezer Shimeles Tesfaye <ebenezertesfaye@yahoo.com>
2023-04-20 07:19:30 +0000

    change /my_qr to be fully bloc

--- lib/view/qr/my_qr.dart
+++ lib/view/qr/my_qr.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:nuvio/blocs/balance_bloc.dart';
 import 'package:nuvio/blocs/keys_bloc.dart';
 import 'package:nuvio/core/common/QrWidget.dart';
@@ -27,70 +28,79 @@ class _MyQrState extends State<MyQr> {
   @override
   Widget build(BuildContext context) {
     final String _myName = "Nablox";
-    final String _myHash =
-        (Provider.of<KeyBloc>(context, listen: false)).publicKey;
-    final String _myAmmount =
-        (Provider.of<BalanceBloc>(context, listen: false)).state.toString();
-    return Scaffold(
-        backgroundColor: Colors.black,
-        appBar: AppBar(
-          backgroundColor: Colors.black,
-          title: const Text("Qr code"),
-          centerTitle: true,
-        ), //TODO change AppBar
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            const Flexible(
-                child: FractionallySizedBox(
-              heightFactor: 0.4,
-            )),
-            Flexible(
-              child: FractionallySizedBox(
-                heightFactor: 0.6,
-                child: Padding(
-                  padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
-                  child: Container(
-                    decoration: const BoxDecoration(
-                        color: Color(0x5B5B5B5B),
-                        borderRadius: BorderRadius.all(Radius.circular(5))),
-                    child: ListTile(
-                      shape: const RoundedRectangleBorder(
-                          borderRadius:
-                              BorderRadius.all(Radius.elliptical(10, 20))),
-                      leading: const SizedBox(),
-                      subtitle: Text(
-                        _hideHalf(_myHash),
-                        style: const TextStyle(color: Colors.white30),
-                      ),
-                      trailing: Text(
-                        "\$$_myAmmount",
-                        style: const TextStyle(
-                            fontWeight: FontWeight.bold, color: Colors.white),
-                      ),
-                      title: Text(
-                        _myName,
-                        style: const TextStyle(
-                            fontWeight: FontWeight.bold, color: Colors.white),
+
+    // final String _myAmmount =
+    //     (Provider.of<BalanceBloc>(context, listen: false)).state.toString();
+    return BlocBuilder<BalanceBloc, double>(builder: (context, myAmmount) {
+      return BlocBuilder<KeyBloc, bool>(builder: (context, __) {
+        final String myHash =
+            (Provider.of<KeyBloc>(context, listen: false)).publicKey;
+        return Scaffold(
+            backgroundColor: Colors.black,
+            appBar: AppBar(
+              backgroundColor: Colors.black,
+              title: const Text("Qr code"),
+              centerTitle: true,
+            ), //TODO change AppBar
+            body: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                const Flexible(
+                    child: FractionallySizedBox(
+                  heightFactor: 0.4,
+                )),
+                Flexible(
+                  child: FractionallySizedBox(
+                    heightFactor: 0.6,
+                    child: Padding(
+                      padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+                      child: Container(
+                        decoration: const BoxDecoration(
+                            color: Color(0x5B5B5B5B),
+                            borderRadius: BorderRadius.all(Radius.circular(5))),
+                        child: ListTile(
+                          shape: const RoundedRectangleBorder(
+                              borderRadius:
+                                  BorderRadius.all(Radius.elliptical(10, 20))),
+                          leading: const SizedBox(),
+                          subtitle: Text(
+                            _hideHalf(myHash),
+                            style: const TextStyle(color: Colors.white30),
+                          ),
+                          trailing: Text(
+                            "\$$myAmmount",
+                            style: const TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white),
+                          ),
+                          title: Text(
+                            _myName,
+                            style: const TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white),
+                          ),
+                        ),
                       ),
                     ),
                   ),
                 ),
-              ),
-            ),
-            const Flexible(
-                child: FractionallySizedBox(
-              heightFactor: 0.4,
-            )),
-            Padding(
-                padding: EdgeInsets.all(20),
-                child: Container(
-                    decoration: const BoxDecoration(
-                        color: Color(0x5B5B5B5B),
-                        borderRadius: BorderRadius.all(Radius.circular(10))),
-                    child: Padding(
-                        padding: EdgeInsets.all(24), child: QrWidget(_myHash))))
-          ],
-        ));
+                const Flexible(
+                    child: FractionallySizedBox(
+                  heightFactor: 0.4,
+                )),
+                Padding(
+                    padding: const EdgeInsets.all(20),
+                    child: Container(
+                        decoration: const BoxDecoration(
+                            color: Color(0x5B5B5B5B),
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(10))),
+                        child: Padding(
+                            padding: const EdgeInsets.all(24),
+                            child: QrWidget(myHash))))
+              ],
+            ));
+      });
+    });
   }
 }
